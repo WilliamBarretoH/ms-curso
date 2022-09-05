a38f3ea0f80d38ebcd03c010b93bbc018cfbd59f
William <willbarretosan@gmail.com>
2022-09-05 11:11:37 -0300

    Implementação basica da entidade Worker

---
+++ hr-worker/src/main/java/com/devsuperior/hrworker/entities/Worker.java
@@ -0,0 +1,70 @@
+package com.devsuperior.hrworker.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, Double dayliIncome) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dayliIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Worker worker = (Worker) o;
+
+        if (!id.equals(worker.id)) return false;
+        if (!name.equals(worker.name)) return false;
+        return dailyIncome.equals(worker.dailyIncome);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + dailyIncome.hashCode();
+        return result;
+    }
+}
